b3ae74de06292d25c0c3322f5aa32f54b912e6a4
Rounak Singh <rounaksingh9431738764@gmail.com>
2023-03-03 18:32:06 +0530

    Completed Modal Part of the App

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 // Local Components
-import Button from './Components/Button';
-import ImageViewer from './Components/ImageViewer';
+import Button from "./Components/Button";
+import ImageViewer from "./Components/ImageViewer";
+import IconButton from "./Components/IconButton";
+import CircleButton from "./Components/CircleButton";
+import EmojiPicker from "./Components/EmojiPicker";
+import EmojiList from "./Components/EmojiList";
+import EmojiSticker from "./Components/EmojiSticker";
 
 // Default Image Path
-const PlaceholderImage = require('./assets/images/background-image.png')
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 /**
  * @returns The ImagePicker Screen of App. Also this is the main screen of our app.
@@ -16,12 +21,31 @@ const PlaceholderImage = require('./assets/images/background-image.png')
  */
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickerEmoji, setPickerEmoji] = useState(null);
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onSaveImageAsync = async () => {
+    return null;
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
   /**
    * Enables the app to pick an image from the user platform.
    * @returns assets array as ImagePickerResult.
    */
-  
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -30,8 +54,9 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
-      alert('You did not select any image.');
+      alert("You did not select any image.");
     }
   };
 
@@ -42,11 +67,38 @@ export default function App() {
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickerEmoji !== null ? (
+          <EmojiSticker imageSize={40} stickerSource={pickerEmoji} />
+        ) : null}
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickerEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
       <StatusBar style="auto" />
     </View>
   );
@@ -55,15 +107,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
   footerContainer: {
-    flex: 1 / 3.5,
-    alignItems: 'center',
-  }
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+  optionContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
 });
--- Components/ImageViewer.js
+++ Components/ImageViewer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image } from "react-native";
 
 /**
  * @param {*} placeholderImageSource : The default image used by the app.
@@ -7,19 +7,16 @@ import { StyleSheet, Image } from 'react-native';
  */
 
 export default function ImageViewer({ placeholderImageSource, selectedImage }) {
-    const imageSource = selectedImage !== null
-        ? { uri: selectedImage }
-        : placeholderImageSource;
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
 
-    return (
-        <Image source={imageSource} style={styles.image} />
-    );
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
-    },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 });
